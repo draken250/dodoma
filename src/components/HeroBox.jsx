df10609fb42d35d7940ad3f6f1abd977c951ac77
mbonikevin <k.mbonimpay@alustudent.com>
2024-11-14 15:04:04 +0200

    hero section

---
+++ src/components/HeroBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeroBox({text, img, position}) {
+  return (
+    <div className="w-full h-full flex bg-red-300 rounded-3xl overflow-hidden relative">
+      <img src={img} className="w-full h-full object-cover object-bottom" />
+      <div className={`absolute top-0 left-0 z-10 w-full h-full ${position === 'top' ? 'bg-gradient-to-br' : 'bg-gradient-to-tr'} to-transparent from-black/65 p-8 space-y-2 flex flex-col items-start ${position === 'top' ? 'justify-start' : 'justify-end'}`}>
+        <h1 className="mb-0 text-3xl font-medium text-white max-w-[180px]">
+           {text}
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default HeroBox;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,24 +1,64 @@
 import React from "react";
+import HeroBox from "./HeroBox";
 
 function HeroSection() {
   return (
-    <div className="space-y-4 bg-white" >
-      <div className="w-full flex h-fit px-6 space-x-4 bg-blue-400">
-        <div className="w-2/3 h-full flex">
+    <div className="space-y-3 py-4">
+      <div className="w-full flex h-[500px] px-6 gap-3">
+        <div className="w-2/3 min-h-full flex flex-col rounded-3xl overflow-hidden relative">
           <img
             src="./assets/mother.jpg"
-            className="w-full h-[500px] object-cover"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-tr to-transparent from-black/45 p-10 space-y-2 flex flex-col items-start justify-end">
+            <h1 className="mb-0 text-5xl font-semibold text-white">
+              Dream Comfort, <br /> Unbeatable Prices!
+            </h1>
+            <p className="mb-0 text-lg max-w-[500px] font-normal text-white">
+              Sleep better every night with comfy, quality mattresses designed
+              to keep you cozy and well-rested.
+            </p>
+            <button className="w-fit px-5 py-2.5 bg-white rounded-full text-black font-medium text-sm">
+              Explore Products
+            </button>
+          </div>
+        </div>
+        <div className="w-1/3 full flex flex-col gap-3">
+          <HeroBox text="Sleek Design" img="./assets/bed2.jpg" position="top" />
+          <HeroBox
+            text="Comfort That Lasts"
+            img="./assets/bed3.jpg"
+            position="top"
           />
         </div>
-        <div className="w-1/3 min-h-full flex flex-col bg-green-300 space-y-4">
-          <div className="w-full h-1/2 flex bg-red-300 p-3"></div>
-          <div className="w-full h-1/2 flex bg-red-300 p-3"></div>
-        </div>
       </div>
-      <div className="w-full flex h-60 px-6 space-x-3 bg-yellow-300">
-        <div className="w-3/4 h-full flex bg-green-300 p-4"></div>
-        <div className="w-full h-full flex bg-red-300 p-3"></div>
-        <div className="w-full h-full flex bg-red-300 p-3"></div>
+      <div className="w-full flex h-60 px-6 space-x-3">
+        <div className="w-3/4 h-full flex flex-col p-4 rounded-2xl overflow-hidden relative">
+          <h1 className="text-3xl font-medium max-w-[400px]">
+            Don’t Wait, Sleep Better Tonight!
+          </h1>
+          <p className="pt-2 mb-5 max-w-[400px]">
+            Shop now and enjoy unbeatable comfort, delivered straight to your
+            door
+          </p>
+          <button className="w-fit px-5 py-2.5 bg-white ring-2 ring-black rounded-full text-black font-medium text-sm">
+            Explore Products
+          </button>
+        </div>
+        <div className="w-3/4 h-full relative">
+          <HeroBox
+            text="Sleek Design"
+            img="./assets/bed4.jpg"
+            position="bottom"
+          />
+        </div>
+        <div className="w-3/4 h-full relative">
+          <HeroBox
+            text="Sleek Design"
+            img="./assets/bed5.jpg"
+            position="bottom"
+          />
+        </div>
       </div>
     </div>
   );
